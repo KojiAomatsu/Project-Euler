2704b55cec463de6db178e4fcad1c796907929a4
Koji Aomatsu <penbook.koji@gmail.com>
2017-04-07 10:18:39 +0900

    solve problem-14

---
+++ problem-14.py
@@ -0,0 +1,21 @@
+def calc(n):
+    result = 1
+    maxi = 1
+    for i in range(1,n):
+        num = i
+        length = 1
+        while num != 1:
+            if num%2 == 0:
+                num = num/2
+            else:
+                num = 3 * num +1
+            length += 1
+        if length > maxi:
+            maxi = length
+            result = i
+    return result
+
+print(calc(100))
+print(calc(1000))
+print(calc(10000))
+print(calc(1000000))
